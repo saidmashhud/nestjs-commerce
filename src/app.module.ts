0584d56cb50dee9287557cc61fe88c8dce83bf09
Saidmashhud Habibzoda <shade251103@gmail.com>
2023-01-30 16:06:44 +0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(''), ProductModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
