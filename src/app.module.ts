3be0f607cd29f8d17057bb35d9e22b09ab331699
Saidmashhud Habibzoda <shade251103@gmail.com>
2023-02-02 16:05:29 +0500

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { CartModule } from './cart/cart.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(''), ProductModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    ProductModule,
+    UserModule,
+    CartModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,13 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { ProductSchema } from './schemas/product.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+  ],
   providers: [ProductService],
   controllers: [ProductController],
 })
